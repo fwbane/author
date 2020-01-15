e221440b0c21a5fb2e52e1401848eb1f163eef32
fbane3 <fwbane@gmail.com>
2020-01-15 12:07:37 -0800

    adding predict method to test

--- glove_keras_cnn.py
+++ glove_keras_cnn.py
@@ -129,6 +129,26 @@ class GloveKerasCnn(Model):
             print(np.argmax(predictions[n]))
             print("")
 
+    def pad_trunc(data, maxlen):
+        new_data = []
+        # Create a vector of 0s the length of our word vectors
+        zero_vector = []
+        for _ in range(len(data[0][0])):
+            zero_vector.append(0.0)
+        for sample in data:
+            if len(sample) > maxlen:
+                temp = sample[:maxlen]
+            elif len(sample) < maxlen:
+                temp = sample
+                # Append the appropriate number 0 vectors to the list
+                additional_elems = maxlen - len(sample)
+                for _ in range(additional_elems):
+                    temp.append(zero_vector)
+            else:
+                temp = sample
+            new_data.append(temp)
+        return new_data
+
 
 
 class VanillaGloveKerasCnn(GloveKerasCnn):
